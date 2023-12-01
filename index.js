a53e716e94c308fa791fbcb1a9cf91b07714b314
Haritha <haritha@fedolab.com>
2023-12-01 06:33:14 +0530

    Created staff dashboard and center incharge attendance

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ app.use(
     changeOrigin: true,
   })
 );
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
